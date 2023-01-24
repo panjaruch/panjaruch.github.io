67abf64f52e797114ec3ec55de993b12ecfb4189
panjaruch <121091398+panjaruch@users.noreply.github.com>
2023-01-24 10:20:00 +0100

    Add files via upload

--- kartka.js
+++ kartka.js
@@ -3,8 +3,39 @@ import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
 // initialize kaboom context
 kaboom();
 
+// wczytywanie zasobów
+
+loadSprite("tlo","swieta.png")
+loadSprite("mikolaj","pierwszyplangotowy.png")
+
+loadSound("muzyka","gdyslicznapanna.mp3")
+
 // add a piece of text at position (120, 80)
 add([
     text("hello"),
     pos(120, 80),
 ]);
+
+add([
+    sprite("tlo"),
+    pos(0,0)
+])
+
+const mikolaj=add([
+    sprite("mikolaj"),
+    pos(100,100)
+])
+
+onUpdate(()=>{
+
+    if(mikolaj.pos.x>500) mikolaj.pos.x = 20
+
+    else mikolaj.pos.x += 10
+
+    
+    
+})
+
+onKeyPress("space", ()=>{
+    play("muzyka")
+})
